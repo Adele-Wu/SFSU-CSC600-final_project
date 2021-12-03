58c3ef9c25cf505bdea7d912e58010f901fd9808
adele-wu <adelewu2000@gmail.com>
2021-12-03 00:51:12 -0800

    add albums and artists to songs

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -9,26 +9,28 @@ PRAGMA foreign_keys;
 CREATE TABLE songs (
 	id int NOT NULL PRIMARY KEY,
 	song_title text NOT NULL,
-	notes varchar NOT NULL,
+	artist text NOT NULL,
+	album text NOT NULL,
+	notes varchar NOT NULL
 );
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+INSERT INTO songs (id, song_title, artist, album, notes) 
+VALUES (1, 'Ode to Joy (Dubstep Remix)', 'Beethoven', 'Symphony No.9', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (2, 'Avatar’s Love – Avatar The Last Airbender OST', 'C4 E4 G4 B4 D4 F4 A4 C5 E4 G4 B4 D5 F4 A4 D5 F5 E5 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4 F4 E4 F4 C4 F4 E4 F4 E4 C4 F4 E4 F4 C4 F4 E4 F4 E4 C4 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4');
+INSERT INTO songs (id, song_title, artist, album, notes) 
+VALUES (2, 'Avatar’s Love', 'Jeremy Zuckerman', 'Avatar The Last Airbender OST', 'C4 E4 G4 B4 D4 F4 A4 C5 E4 G4 B4 D5 F4 A4 D5 F5 E5 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4 F4 E4 F4 C4 F4 E4 F4 E4 C4 F4 E4 F4 C4 F4 E4 F4 E4 C4 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4 C5 B4 G4 E4');
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (3, 'Twinkle Twinkle Little Star', 'C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4 G4 G4 F4 F4 E4 E4 D4 G4 G4 F4 F4 E4 E4 D4 C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4');
+INSERT INTO songs (id, song_title, artist, album, notes) 
+VALUES (3, 'Twinkle Twinkle Little Star', 'Jane Taylor', 'lullaby', 'C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4 G4 G4 F4 F4 E4 E4 D4 G4 G4 F4 F4 E4 E4 D4 C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4');
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (4, 'Jingle Bells', 'E4 E4 E4 E4 E4 E4 E4 G4 C4 D4 E4 F4 F4 F4 F4 F4 E4 E4 E4 E4 E4 D4 D4 E4 D4 G4 E4 E4 E4 E4 E4 E4 E4 G4 C4 D4 E4 F4 F4 F4 F4 F4 E4 E4 E4 E4 G4 G4 F4 D4 C4');
+INSERT INTO songs (id, song_title, artist, album, notes) 
+VALUES (4, 'Jingle Bells', 'James Pierpont', 'Christmas', 'E4 E4 E4 E4 E4 E4 E4 G4 C4 D4 E4 F4 F4 F4 F4 F4 E4 E4 E4 E4 E4 D4 D4 E4 D4 G4 E4 E4 E4 E4 E4 E4 E4 G4 C4 D4 E4 F4 F4 F4 F4 F4 E4 E4 E4 E4 G4 G4 F4 D4 C4');
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (5, 'Stay - The Kid Laroi, Justin Bieber', 'B2 F1 E2 E2 E2 C3 E2 E2 B2 F1 E2 E2 E2 C3 E2 E2');
+INSERT INTO songs (id, song_title, artist, album, notes) 
+VALUES (5, 'Stay', 'The Kid Laroi, Justin Bieber', 'F*ck Love 3: Over You', 'B2 F1 E2 E2 E2 C3 E2 E2 B2 F1 E2 E2 E2 C3 E2 E2');
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (6, 'Peaches - Justin Bieber', 'D4 D4 D4 D4 D4 D4 C4 E4 C4 A3 C4 C4 D4 D4 D4 D4 D4 D4 C4 E4 C4 A3 C4 C4 G4 F4 E4 D4 D4 D4 C4 E4 C4 C4 C4 C4 G4 F4 E4 D4 D4 D4 C4 E4 C4 C4 C4 C4 C4 D4 E4 E4 D4 F4 E4 D4 C4 D4 D4 D4 E4 D4 C4 B3 C4 B3 A3');
+INSERT INTO songs (id, song_title, artist, album, notes) 
+VALUES (6, 'Peaches', 'Justin Bieber', 'Justic', 'D4 D4 D4 D4 D4 D4 C4 E4 C4 A3 C4 C4 D4 D4 D4 D4 D4 D4 C4 E4 C4 A3 C4 C4 G4 F4 E4 D4 D4 D4 C4 E4 C4 C4 C4 C4 G4 F4 E4 D4 D4 D4 C4 E4 C4 C4 C4 C4 C4 D4 E4 E4 D4 F4 E4 D4 C4 D4 D4 D4 E4 D4 C4 B3 C4 B3 A3');
 
-INSERT INTO songs (id, song_title, notes) 
-VALUES (7, 'Come Get Her', 'E3 A4 B4 C4 E3 E3 F3 F3 F3 E3 D4 E3 E3 A4 B4 C4 E3 E3 F3 F3 F3 E3 D4 E3');
+INSERT INTO songs (id, song_title, artist, album, notes) 
+VALUES (7, 'Come Get Her', 'Rae Sremmurd', 'SremmLife', 'E3 A4 B4 C4 E3 E3 F3 F3 F3 E3 D4 E3 E3 A4 B4 C4 E3 E3 F3 F3 F3 E3 D4 E3');
