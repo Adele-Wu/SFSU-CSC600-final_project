f4387cf2d79d27ea7ff4b7e1332d652c7a0c0afa
DeathsValentine <39684769+DeathsValentine@users.noreply.github.com>
2021-12-05 22:14:47 -0800

    Added filter dropdown

--- client/src/search.tsx
+++ client/src/search.tsx
@@ -3,6 +3,12 @@ const Search = () => (
       <input type="text" id="header-search" placeholder="Search Songs" name="s" />
       <button type="submit">Search</button>
       <button type="reset">Clear</button>
+      <select name="Filters" id="Filters">
+        <option disabled selected>Filters</option>
+        <option value="song">Song</option>
+        <option value="artist">Artist</option>
+        <option value="album">Album</option>
+      </select>
     </form>
   );
 
